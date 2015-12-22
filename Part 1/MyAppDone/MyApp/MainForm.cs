afd834c7dd02632712246b7b8067ecfb016c2438
Kyrylo M <kirmir@outlook.com>
2015-12-22 22:59:23 +0200

    Added ready Install.

---
+++ Part 1/MyAppDone/MyApp/MainForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace MyApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            uxProduct.Text = Assembly.GetEntryAssembly().GetCustomAttributes<AssemblyProductAttribute>().First().Product;
+            uxVersion.Text = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            uxCompany.Text = Assembly.GetEntryAssembly().GetCustomAttributes<AssemblyCompanyAttribute>().First().Company;
+        }
+    }
+}
